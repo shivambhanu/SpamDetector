dd72f6aed2af37f2c74b06cb8389101b52148faf
shivambhanu <shivambhanu6924@gmail.com>
2023-12-28 20:55:14 +0530

    Used generic class-based views for more simplicity

--- SpamDetectorAPI/views.py
+++ SpamDetectorAPI/views.py
@@ -6,6 +6,7 @@ from .models import User
 from .serializers import UserSerializer
 
 from rest_framework.views import APIView
+from rest_framework import generics
 
 
 
@@ -55,42 +56,11 @@ from rest_framework.views import APIView
 #                                 CLASS-BASED VIEWS                                          #
 #=============================================================================================
 #Class based views
-class UsersView(APIView):
-    def get(self, request):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class UsersView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
-
-class SingleUserView(APIView):
-    def get_object(self, request, pk):
-        try:
-            return User.objects.get(pk=pk)
-        except User.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    def get(self, request, pk):
-        user = self.get_object(pk)
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        user = self.get_object(pk)
-        serializer = UserSerializer(user, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk):
-        user = self.get_object(pk)
-        user.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class SingleUserView(generics.RetrieveUpdateDestroyAPIView):
+    querysert = User.objects.all()
+    serializer_class = UserSerializer
