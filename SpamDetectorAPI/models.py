355481c00ff07709200357688f7e32667535cb20
shivambhanu <shivambhanu6924@gmail.com>
2023-12-28 17:59:39 +0530

    Created Models for users, contacts and phone-nums

--- SpamDetectorAPI/models.py
+++ SpamDetectorAPI/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.CharField(primary_key=True, max_length=15)
+    email = models.EmailField(max_length=255, blank=True, null=True)
+    
+    def __str__(self) -> str:
+        return self.name
+
+
+
+# class Contacts(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     contact_name = models.CharField(max_length=255)
+#     contact_phone = models.CharField(max_length=15)
+    
+#     def __str__(self) -> str:
+#         return (f"{self.user__name}: {self.contact_name}")
+
+
+
+# class PhoneBook(models.Model):
+#     phone = models.CharField(max_length=255)
+#     def __str__(self) -> str:
+#         return self.phone
